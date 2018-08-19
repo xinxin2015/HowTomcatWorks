6e4bdd1f8a099402efd150e9bc26de05864f9d30
1178919633@qq.com <1178919633@qq.com>
2018-08-19 22:37:16 +0800

    .

---
+++ ex01/src/test/java/App.java
@@ -0,0 +1,10 @@
+
+
+public class App {
+
+    public static void main(String[] args) {
+        System.out.println("Hello");
+        System.out.println(System.getProperty("user.dir"));
+    }
+    
+}
